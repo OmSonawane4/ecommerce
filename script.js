e402e53f4775104ba3ace39ffd76560eb78df190
Om Sonawane <148892933+OmSonawane4@users.noreply.github.com>
2024-02-22 12:57:40 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const addToCartButtons = document.querySelectorAll('.product button');
+
+  addToCartButtons.forEach(button => {
+      button.addEventListener('click', () => {
+          alert('Product added to cart');
+      });
+  });
+});
